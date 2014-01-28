b04a9d9bb4f4054902f1ec9a0570cc8603f1b669
MBrasebin <MBrasebin@67b3e15a-eb92-4fe2-b8f7-2fed31984fbb>
2014-01-28 14:16:38 +0000

    git-svn-id: https://forge-cogit.ign.fr/cogit-depot/geoxygene-cogit/trunk/modules/library/simplu3d@4221 67b3e15a-eb92-4fe2-b8f7-2fed31984fbb

--- src/main/java/fr/ign/cogit/simplu3d/exec/analysis/TestProjectedShadow.java
+++ src/main/java/fr/ign/cogit/simplu3d/exec/analysis/TestProjectedShadow.java
@@ -13,7 +13,6 @@ import fr.ign.cogit.geoxygene.api.spatial.geomroot.IGeometry;
 import fr.ign.cogit.geoxygene.contrib.geometrie.Vecteur;
 import fr.ign.cogit.geoxygene.feature.DefaultFeature;
 import fr.ign.cogit.geoxygene.feature.FT_FeatureCollection;
-import fr.ign.cogit.geoxygene.sig3d.calculation.Calculation3D;
 import fr.ign.cogit.geoxygene.sig3d.equation.PlanEquation;
 import fr.ign.cogit.geoxygene.sig3d.gui.MainWindow;
 import fr.ign.cogit.geoxygene.sig3d.representation.ConstantRepresentation;
@@ -24,9 +23,9 @@ import fr.ign.cogit.geoxygene.spatial.coordgeom.DirectPosition;
 import fr.ign.cogit.geoxygene.spatial.coordgeom.DirectPositionList;
 import fr.ign.cogit.geoxygene.spatial.coordgeom.GM_LineString;
 import fr.ign.cogit.geoxygene.spatial.coordgeom.GM_Polygon;
+import fr.ign.cogit.sig3d.calculation.raycasting.InverseProjection;
+import fr.ign.cogit.sig3d.calculation.raycasting.ProjectedShadow;
 import fr.ign.cogit.sig3d.calculation.raycasting.RayCasting;
-import fr.ign.cogit.simplu3d.analysis.InverseProjection;
-import fr.ign.cogit.simplu3d.analysis.ProjectedShadow;
 import fr.ign.cogit.simplu3d.io.load.application.LoaderSHP;
 import fr.ign.cogit.simplu3d.model.application.Environnement;
 import fr.ign.cogit.simplu3d.representation.RepEnvironnement;
@@ -34,143 +33,137 @@ import fr.ign.cogit.simplu3d.representation.RepEnvironnement.Theme;
 
 public class TestProjectedShadow {
 
-	public static void main(String[] args) throws CloneNotSupportedException {
+  public static void main(String[] args) throws CloneNotSupportedException {
 
-		RayCasting.EPSILON = 0.01;
-		RayCasting.CHECK_IS_ON_EDGE = true;
+    RayCasting.EPSILON = 0.01;
+    RayCasting.CHECK_IS_ON_EDGE = true;
 
-		PlanEquation.EPSILON = 0.0000000000001;
+    PlanEquation.EPSILON = 0.0000000000001;
 
-		ConstantRepresentation.backGroundColor = new Color(156, 180, 193);
+    ConstantRepresentation.backGroundColor = new Color(156, 180, 193);
 
-		String folder = "D:/Donnees/StrasbourgKIT/StrasbourgKIT/GTRU/Project1/";
+    String folder = "E:/mbrasebin/Donnees/Strasbourg/GTRU/Project1/";
 
-		Environnement env = LoaderSHP.load(folder);
+    Environnement env = LoaderSHP.load(folder);
 
-		List<Theme> lTheme = new ArrayList<RepEnvironnement.Theme>();
-		lTheme.add(Theme.TOIT_BATIMENT);
-		lTheme.add(Theme.FACADE_BATIMENT);
-		//lTheme.add(Theme.FAITAGE);
-		//lTheme.add(Theme.PIGNON);
-		//lTheme.add(Theme.GOUTTIERE);
-		//lTheme.add(Theme.VOIRIE);
-		// lTheme.add(Theme.PARCELLE);
-		lTheme.add(Theme.SOUS_PARCELLE);
-		// lTheme.add(Theme.ZONE);
-		//lTheme.add(Theme.PAN);
+    List<Theme> lTheme = new ArrayList<RepEnvironnement.Theme>();
+    lTheme.add(Theme.TOIT_BATIMENT);
+    lTheme.add(Theme.FACADE_BATIMENT);
+    // lTheme.add(Theme.FAITAGE);
+    // lTheme.add(Theme.PIGNON);
+    // lTheme.add(Theme.GOUTTIERE);
+    // lTheme.add(Theme.VOIRIE);
+    // lTheme.add(Theme.PARCELLE);
+    lTheme.add(Theme.SOUS_PARCELLE);
+    // lTheme.add(Theme.ZONE);
+    // lTheme.add(Theme.PAN);
 
-		Theme[] tab = lTheme.toArray(new Theme[0]);
+    Theme[] tab = lTheme.toArray(new Theme[0]);
 
-		List<VectorLayer> vl = RepEnvironnement.represent(env, tab);
+    List<VectorLayer> vl = RepEnvironnement.represent(env, tab);
 
-		MainWindow mW = new MainWindow();
+    MainWindow mW = new MainWindow();
 
-		for (VectorLayer l : vl) {
+    for (VectorLayer l : vl) {
 
-			mW.getInterfaceMap3D().getCurrent3DMap().addLayer(l);
-		}
+      mW.getInterfaceMap3D().getCurrent3DMap().addLayer(l);
+    }
 
-		mW.getInterfaceMap3D().removeLight(0);
+    mW.getInterfaceMap3D().removeLight(0);
 
-		/*
-		 * mW.getInterfaceMap3D().addLight(new Color(147,147,147), 0,0,0);
-		 * mW.getInterfaceMap3D().moveLight(180, -15, 120,0);
-		 * mW.getInterfaceMap3D().addLight(new Color(147,147,147), 0,0,0);
-		 * mW.getInterfaceMap3D().moveLight( -140, 3, 120,1);
-		 */
+    /*
+     * mW.getInterfaceMap3D().addLight(new Color(147,147,147), 0,0,0);
+     * mW.getInterfaceMap3D().moveLight(180, -15, 120,0);
+     * mW.getInterfaceMap3D().addLight(new Color(147,147,147), 0,0,0);
+     * mW.getInterfaceMap3D().moveLight( -140, 3, 120,1);
+     */
 
-		// 1051042.8513268954120576,6840539.0837931865826249 :
-		// 1051264.8064121364150196,6840679.2711814027279615
+    // 1051042.8513268954120576,6840539.0837931865826249 :
+    // 1051264.8064121364150196,6840679.2711814027279615
 
-		double xc = (1051042.8513268954120576 + 1051264.8064121364150196) / 2;
-		double yc = (6840539.0837931865826249 + 6840679.2711814027279615) / 2;
+    double xc = (1051042.8513268954120576 + 1051264.8064121364150196) / 2;
+    double yc = (6840539.0837931865826249 + 6840679.2711814027279615) / 2;
 
-		double z = 138;
+    double z = 138;
 
-		double longueur = 1051264.8064121364150196 - 1051042.8513268954120576;
-		double largeur = 6840679.2711814027279615 - 6840539.0837931865826249;
+    double longueur = 1051264.8064121364150196 - 1051042.8513268954120576;
+    double largeur = 6840679.2711814027279615 - 6840539.0837931865826249;
 
-		IDirectPositionList dpl = new DirectPositionList();
+    IDirectPositionList dpl = new DirectPositionList();
 
-		IDirectPosition dp1 = new DirectPosition(xc - longueur / 2, yc
-				- largeur / 2, z);
-		IDirectPosition dp2 = new DirectPosition(xc + longueur / 2, yc
-				- largeur / 2, z);
-		IDirectPosition dp3 = new DirectPosition(xc + longueur / 2, yc
-				+ largeur / 2, z);
-		IDirectPosition dp4 = new DirectPosition(xc - longueur / 2, yc
-				+ largeur / 2, z);
+    IDirectPosition dp1 = new DirectPosition(xc - longueur / 2, yc - largeur
+        / 2, z);
+    IDirectPosition dp2 = new DirectPosition(xc + longueur / 2, yc - largeur
+        / 2, z);
+    IDirectPosition dp3 = new DirectPosition(xc + longueur / 2, yc + largeur
+        / 2, z);
+    IDirectPosition dp4 = new DirectPosition(xc - longueur / 2, yc + largeur
+        / 2, z);
 
-		dpl.add(dp1);
-		dpl.add(dp2);
-		dpl.add(dp3);
-		dpl.add(dp4);
-		dpl.add(dp1);
+    dpl.add(dp1);
+    dpl.add(dp2);
+    dpl.add(dp3);
+    dpl.add(dp4);
+    dpl.add(dp1);
 
-		IPolygon emprise = new GM_Polygon(new GM_LineString(dpl));
+    IPolygon emprise = new GM_Polygon(new GM_LineString(dpl));
 
-		Calculation3D.translate(emprise, -Environnement.dpTranslate.getX(),
-				-Environnement.dpTranslate.getY(), 0);
+    if (emprise.isEmpty()) {
+      System.out.println("null");
+    }
 
-		IFeatureCollection<IFeature> fc = new FT_FeatureCollection<IFeature>();
+    // Calculation3D.translate(emprise, -Environnement.dpTranslate.getX(),
+    // -Environnement.dpTranslate.getY(), 0);
 
-		IFeature feat = new DefaultFeature(emprise);
+    IFeatureCollection<IFeature> fc = new FT_FeatureCollection<IFeature>();
 
-		fc.add(feat);
+    IFeature feat = new DefaultFeature(emprise);
 
-		// feat.setRepresentation(new Object2d(feat, Color.pink));
+    fc.add(feat);
 
-		feat.setRepresentation(new TexturedSurface(
-				feat,
-				TextureManager
-						.textureLoading("D:/Donnees/StrasbourgKIT/StrasbourgKIT/Texture/Env3D_86.png"),
-				longueur, largeur));
+    // feat.setRepresentation(new Object2d(feat, Color.pink));
 
-		mW.getInterfaceMap3D().getCurrent3DMap()
-				.addLayer(new VectorLayer(fc, "Cool"));
+    feat.setRepresentation(new TexturedSurface(feat, TextureManager
+        .textureLoading(folder + "background3D.png"), longueur, largeur));
 
-		long t = System.currentTimeMillis();
+    mW.getInterfaceMap3D().getCurrent3DMap()
+        .addLayer(new VectorLayer(fc, "Cool"));
 
-		
+    long t = System.currentTimeMillis();
 
-		List<IGeometry> lGeom2 = ProjectedShadow.process(env.getBuildings(), env
-				.getSubParcels(), (IPolygon) feat.getGeom(), new Vecteur(0,
-				1, -0.4), -2, 15,
-				ProjectedShadow.POSSIBLE_RESULT.PROJECTED_VOLUME, true); 
-		
-		List<IGeometry> lGeom = InverseProjection.process(env.getBuildings(), 4, new Vecteur(0,1, -0.4), 20);
-		
+    List<IGeometry> lGeom2 = ProjectedShadow.process(env.getBuildings(),
+        env.getSubParcels(), (IPolygon) feat.getGeom(),
+        new Vecteur(0, 1, -0.4), -2, 15,
+        ProjectedShadow.POSSIBLE_RESULT.PROJECTED_VOLUME, true);
 
-		
-		System.out.println("Temps écoulé " + (System.currentTimeMillis() - t));
+    List<IGeometry> lGeom = InverseProjection.process(env.getBuildings(), 4,
+        new Vecteur(0, 1, -0.4), 20);
 
-		IFeatureCollection<IFeature> lfeatC = new FT_FeatureCollection<IFeature>();
-		IFeatureCollection<IFeature> lfeatC2 = new FT_FeatureCollection<IFeature>();
-		
-		
-		//List<IOrientableCurve> lC = CalculNormales.getNormal(env.getBatiments(), 5);
-		//lGeom.addAll(lC);
+    System.out.println("Temps écoulé " + (System.currentTimeMillis() - t));
 
-		for (IGeometry geom : lGeom2) {
-			lfeatC2.add(new DefaultFeature(geom));
-		}
-		
-		for (IGeometry geom : lGeom) {
-			lfeatC.add(new DefaultFeature(geom));
-		}
+    IFeatureCollection<IFeature> lfeatC = new FT_FeatureCollection<IFeature>();
+    IFeatureCollection<IFeature> lfeatC2 = new FT_FeatureCollection<IFeature>();
 
-		System.out.println("NB Point : " + lfeatC.size());
+    // List<IOrientableCurve> lC = CalculNormales.getNormal(env.getBatiments(),
+    // 5);
+    // lGeom.addAll(lC);
 
-		mW.getInterfaceMap3D().getCurrent3DMap()
-				.addLayer(new VectorLayer(lfeatC, "Points", Color.pink));
-		
-		mW.getInterfaceMap3D().getCurrent3DMap()
-		.addLayer(new VectorLayer(lfeatC2, "Points2", Color.pink));
+    for (IGeometry geom : lGeom2) {
+      lfeatC2.add(new DefaultFeature(geom));
+    }
 
+    for (IGeometry geom : lGeom) {
+      lfeatC.add(new DefaultFeature(geom));
+    }
 
+    System.out.println("NB Point : " + lfeatC.size());
 
-		
-		
-	}
+    mW.getInterfaceMap3D().getCurrent3DMap()
+        .addLayer(new VectorLayer(lfeatC, "Points", Color.pink));
+
+    mW.getInterfaceMap3D().getCurrent3DMap()
+        .addLayer(new VectorLayer(lfeatC2, "Points2", Color.pink));
+
+  }
 
 }
