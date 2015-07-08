cb7d927e3884c1d3e455e0626a92925f25f40342
Julien Perret <julien.perret@gmail.com>
2015-07-08 17:46:43 +0200

    Added Parallel Polygon Transform

---
+++ src/main/java/fr/ign/cogit/simplu3d/rjmcmc/cuboid/transformation/birth/ParallelPolygonTransform.java
@@ -0,0 +1,97 @@
+package fr.ign.cogit.simplu3d.rjmcmc.cuboid.transformation.birth;
+
+import java.util.Vector;
+
+import org.apache.log4j.Logger;
+
+import com.vividsolutions.jts.algorithm.Angle;
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.LineString;
+import com.vividsolutions.jts.geom.MultiLineString;
+import com.vividsolutions.jts.operation.distance.DistanceOp;
+
+import fr.ign.cogit.geoxygene.api.spatial.geomroot.IGeometry;
+import fr.ign.cogit.geoxygene.util.conversion.AdapterFactory;
+import fr.ign.geometry.transform.PolygonTransform;
+import fr.ign.rjmcmc.kernel.Transform;
+
+/**
+ * 
+ * This software is released under the licence CeCILL
+ * 
+ * see LICENSE.TXT
+ * 
+ * see <http://www.cecill.info/ http://www.cecill.info/
+ * 
+ * @copyright IGN
+ * 
+ * @version 1.0
+ **/
+public class ParallelPolygonTransform implements Transform {
+  /**
+   * Logger.
+   */
+  static Logger LOGGER = Logger.getLogger(ParallelPolygonTransform.class.getName());
+
+  private double absJacobian[];
+  private PolygonTransform polygonTransform;
+  private MultiLineString limits;
+  private GeometryFactory factory = new GeometryFactory();
+
+  private double deltaLength;
+  private double deltaHeight;
+  private double rangeLength;
+  private double rangeHeight;
+
+  public ParallelPolygonTransform(Vector<Double> d, Vector<Double> v, IGeometry polygon, IGeometry[] limits)
+      throws Exception {
+    this.rangeLength = d.get(2);
+    this.rangeHeight = d.get(4);
+    this.deltaLength = v.get(2);
+    this.deltaHeight = v.get(4);
+    double determinant = rangeLength * rangeHeight;
+    LineString[] lineStrings = new LineString[limits.length];
+    for (int i = 0; i < limits.length; i++) {
+      lineStrings[i] = (LineString) AdapterFactory.toGeometry(factory, limits[i]);
+    }
+    this.limits = factory.createMultiLineString(lineStrings);
+    Geometry pp = AdapterFactory.toGeometry(factory, polygon);
+    this.polygonTransform = new PolygonTransform(pp.intersection(this.limits.buffer(d.get(3) / 2 + v.get(3))), 0.1);
+    this.absJacobian = new double[2];
+    this.absJacobian[0] = Math.abs(determinant) * this.polygonTransform.getAbsJacobian(true);
+    this.absJacobian[1] = Math.abs(1 / determinant) * this.polygonTransform.getAbsJacobian(false);
+  }
+
+  @Override
+  public double apply(boolean direct, Vector<Double> val0, Vector<Double> var0, Vector<Double> val1, Vector<Double> var1) {
+    double pt = this.polygonTransform.apply(direct, val0, var0, val1, var1);
+    if (direct) {
+      Coordinate p = new Coordinate(val1.get(0), val1.get(1));
+      DistanceOp op = new DistanceOp(this.limits, factory.createPoint(p));
+      Coordinate projected = op.nearestPoints()[0];
+      double distance = op.distance();
+      double orientation = Angle.angle(p, projected);
+      val1.set(2, var0.get(2) * rangeLength + deltaLength);
+      val1.set(3, distance * 2);
+      val1.set(4, var0.get(3) * rangeHeight + deltaHeight);
+      val1.set(5, orientation + Math.PI / 2);
+      return pt * this.absJacobian[0];
+    } else {
+      var1.set(2, (val0.get(2) - deltaLength) / rangeLength);
+      var1.set(3, (val0.get(4) - deltaHeight) / rangeHeight);
+      return pt * this.absJacobian[1];
+    }
+  }
+
+  @Override
+  public double getAbsJacobian(boolean direct) {
+    return this.absJacobian[direct ? 0 : 1];
+  }
+
+  @Override
+  public int dimension(int n0, int n1) {
+    return 4;
+  }
+}
---
+++ src/test/java/fr/ign/cogit/simplu3d/test/rjmcmc/cuboid/transformation/birth/ParallelPolygonTransformTest.java
@@ -0,0 +1,147 @@
+package fr.ign.cogit.simplu3d.test.rjmcmc.cuboid.transformation.birth;
+
+import java.util.Vector;
+
+import org.apache.commons.math3.random.RandomGenerator;
+import org.junit.Before;
+import org.junit.Test;
+
+import fr.ign.cogit.geoxygene.api.spatial.coordgeom.ILineString;
+import fr.ign.cogit.geoxygene.api.spatial.coordgeom.IPolygon;
+import fr.ign.cogit.geoxygene.util.conversion.WktGeOxygene;
+import fr.ign.cogit.simplu3d.rjmcmc.cuboid.geometry.impl.Cuboid;
+import fr.ign.cogit.simplu3d.rjmcmc.cuboid.transformation.birth.ParallelPolygonTransform;
+import fr.ign.random.Random;
+
+public class ParallelPolygonTransformTest {
+  ParallelPolygonTransform t;
+
+  @Before
+  public void setUp() throws Exception {
+    // IPolygon polygon = (IPolygon) WktGeOxygene
+    // .makeGeOxygene("POLYGON (( 5 0, 5 -10, 15 -10, 15 -5, 10 -5, 10 0, 5 0 ))");
+    IPolygon polygon = (IPolygon) WktGeOxygene
+        .makeGeOxygene("POLYGON (( 24.46264278127928 -13.32945321889124, 16.83388492956696 -2.4038452403082764, 21.54914732032382 4.036513147066944, 30.596317435922344 9.7484976692033, 35.119902493721604 1.4680368854351595, 23.657597982857375 1.1996886192945253, 32.66643263186438 -8.729197227908939, 41.17690621518163 -1.7138068416609316, 39.98850675084454 -16.47296147939581, 20.092399589846092 -22.60663613403888, 13.307022003147196 -13.942820684355546, 24.46264278127928 -13.32945321889124 ))");
+    // ILineString limits = (ILineString)
+    // WktGeOxygene.makeGeOxygene("LINESTRING ( 5 -10, 15 -10 )");
+    ILineString limit1 = (ILineString) WktGeOxygene
+        .makeGeOxygene("LINESTRING ( 41.17690621518163 -1.7138068416609316, 39.98850675084454 -16.47296147939581, 20.092399589846092 -22.60663613403888 )");
+    ILineString limit2 = (ILineString) WktGeOxygene
+        .makeGeOxygene("LINESTRING ( 30.596317435922344 9.7484976692033, 21.54914732032382 4.036513147066944, 16.83388492956696 -2.4038452403082764 )");
+    double[] d = new double[] { 0, 0, 1, 3, 3, 0 };
+    double[] v = new double[] { 0, 0, 5, 0, 30, 0 };
+
+    Vector<Double> ld = new Vector<>();
+    Vector<Double> lv = new Vector<>();
+
+    for (int i = 0; i < d.length; i++) {
+      ld.add(d[i]);
+    }
+
+    for (int i = 0; i < v.length; i++) {
+      lv.add(v[i]);
+    }
+
+    t = new ParallelPolygonTransform(ld, lv, polygon, new ILineString[] { limit1, limit2 });
+  }
+
+  @Test
+  public void testApply() {
+    // int dimension = 6;
+    for (int index = 0; index < 100; index++) {
+      Vector<Double> lvalIn = new Vector<Double>();
+      Vector<Double> lvarIn = new Vector<Double>();
+      RandomGenerator generator = Random.random();
+      lvarIn.add(generator.nextDouble());
+      lvarIn.add(generator.nextDouble());
+      lvarIn.add(generator.nextDouble());
+      lvarIn.add(generator.nextDouble());
+
+      Vector<Double> lvalOut = new Vector<>();
+      lvalOut.setSize(6);
+
+      Vector<Double> lvarOut = new Vector<>();
+      t.apply(true, lvalIn, lvarIn, lvalOut, lvarOut);
+
+      // String outString = "";
+      // for (int i = 0; i < dimension; i++) {
+      // outString += lvalOut.get(i) + " ";
+      // }
+      // System.out.println("out = " + outString);
+      Cuboid cuboidOut = new Cuboid(lvalOut.get(0), lvalOut.get(1), lvalOut.get(2), lvalOut.get(3), lvalOut.get(4),
+          lvalOut.get(5));
+
+      System.out.println(cuboidOut.toGeometry());
+    }
+    // lvalIn = new Vector<Double>();
+    // lvarIn = new Vector<Double>();
+    // lvarIn.add(generator.nextDouble());
+    // lvarIn.add(generator.nextDouble());
+    // lvarIn.add(generator.nextDouble());
+    // lvarIn.add(generator.nextDouble());
+    //
+    // lvalOut = new Vector<>();
+    // lvalOut.setSize(6);
+    //
+    // lvarOut = new Vector<>();
+    // t.apply(true, lvalIn, lvarIn, lvalOut, lvarOut);
+    //
+    // outString = "";
+    // for (int i = 0; i < dimension; i++) {
+    // outString += lvalOut.get(i) + " ";
+    // }
+    // // System.out.println("out = " + outString);
+    // cuboidOut = new Cuboid(lvalOut.get(0), lvalOut.get(1), lvalOut.get(2),
+    // lvalOut.get(3), lvalOut.get(4),
+    // lvalOut.get(5));
+    // System.out.println(cuboidOut.toGeometry());
+    // lvalIn.clear();
+    // lvarIn.clear();
+    // lvarIn.add(generator.nextDouble());
+    // lvarIn.add(generator.nextDouble());
+    // lvarIn.add(generator.nextDouble());
+    // lvarIn.add(generator.nextDouble());
+    //
+    // lvalOut.clear();
+    // lvalOut.setSize(6);
+    // lvarOut.clear();
+    // t.apply(true, lvalIn, lvarIn, lvalOut, lvarOut);
+    // outString = "";
+    // for (int i = 0; i < dimension; i++) {
+    // outString += lvalOut.get(i) + " ";
+    // }
+    // // System.out.println("out = " + outString);
+    // cuboidOut = new Cuboid(lvalOut.get(0), lvalOut.get(1), lvalOut.get(2),
+    // lvalOut.get(3), lvalOut.get(4),
+    // lvalOut.get(5));
+    // System.out.println(cuboidOut.toGeometry());
+    // GeometryFactory f = new GeometryFactory();
+    // for (int i = 0; i < 1; i++) {
+    // lvalIn = new Vector<Double>();
+    // lvarIn = new Vector<Double>();
+    // lvarIn.add(generator.nextDouble());
+    // lvarIn.add(generator.nextDouble());
+    // lvarIn.add(generator.nextDouble());
+    // lvarIn.add(generator.nextDouble());
+    //
+    // lvalOut = new Vector<>();
+    // lvalOut.setSize(6);
+    //
+    // lvarOut = new Vector<>();
+    // t.apply(true, lvalIn, lvarIn, lvalOut, lvarOut);
+    // // System.out.println(f.createPoint(new Coordinate(lvalOut.get(0),
+    // // lvalOut.get(1))));
+    // }
+  }
+
+  @Test
+  public void testInverse() {
+    // fail("Not yet implemented"); // TODO
+  }
+
+  @Test
+  public void testDimension() {
+    // fail("Not yet implemented"); // TODO
+  }
+
+}
