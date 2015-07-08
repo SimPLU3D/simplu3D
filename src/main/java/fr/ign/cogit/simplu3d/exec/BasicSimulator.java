2853ff45bf05311cd6c3e0d75a5daafdd2e04c7b
Mickaël Brasebin <mickael.brasebin@ign.fr>
2015-07-08 11:26:22 +0200

    - add of iauidf xml file

--- src/main/java/fr/ign/cogit/simplu3d/exec/BasicSimulator.java
+++ src/main/java/fr/ign/cogit/simplu3d/exec/BasicSimulator.java
@@ -17,7 +17,6 @@ import fr.ign.cogit.simplu3d.model.application.Environnement;
 import fr.ign.cogit.simplu3d.rjmcmc.cuboid.geometry.convert.GenerateSolidFromCuboid;
 import fr.ign.cogit.simplu3d.rjmcmc.cuboid.geometry.impl.Cuboid;
 import fr.ign.cogit.simplu3d.rjmcmc.cuboid.optimizer.classconstrained.OptimisedBuildingsCuboidFinalDirectRejection;
-import fr.ign.cogit.simplu3d.rjmcmc.cuboid.predicate.SamplePredicate;
 import fr.ign.cogit.simplu3d.rjmcmc.cuboid.predicate.SamplePredicate2;
 import fr.ign.mpp.configuration.BirthDeathModification;
 import fr.ign.mpp.configuration.GraphConfiguration;
@@ -46,83 +45,86 @@ import fr.ign.parameters.Parameters;
  */
 public class BasicSimulator {
 
-  /**
-   * @param args
-   */
+	/**
+	 * @param args
+	 */
 
-  // [building_footprint_rectangle_cli_main
-  public static void main(String[] args) throws Exception {
+	// [building_footprint_rectangle_cli_main
+	public static void main(String[] args) throws Exception {
 
-    //Chargement du fichier de configuration
-    String folderName = BasicSimulator.class.getClassLoader()
-        .getResource("scenario/").getPath();
-    String fileName = "building_parameters_project_expthese_3.xml";
-    Parameters p = Parameters.unmarshall(new File(folderName + fileName));
+		// Chargement du fichier de configuration
+		String folderName = BasicSimulator.class.getClassLoader()
+				.getResource("scenario/").getPath();
+		String fileName = "building_parameters_project_expthese_3.xml";
+		Parameters p = Parameters.unmarshall(new File(folderName + fileName));
 
-    //Chargement de l'environnement
-    Environnement env = LoadDefaultEnvironment.getENVDEF();
+		// Chargement de l'environnement
+		Environnement env = LoadDefaultEnvironment.getENVDEF();
 
-    //Récupération de l'unité foncière surlaquelle bâtir
-    //J'ai mis 8 car c'est la plus grosse et on sera sur d'avoir des bâtiments
-    BasicPropertyUnit bPU = env.getBpU().get(8);
+		// Récupération de l'unité foncière surlaquelle bâtir
+		// J'ai mis 8 car c'est la plus grosse et on sera sur d'avoir des
+		// bâtiments
+		BasicPropertyUnit bPU = env.getBpU().get(8);
 
-    //Création du Sampler (qui va générer les propositions de solutions)
-    OptimisedBuildingsCuboidFinalDirectRejection oCB = new OptimisedBuildingsCuboidFinalDirectRejection();
+		// Création du Sampler (qui va générer les propositions de solutions)
+		OptimisedBuildingsCuboidFinalDirectRejection oCB = new OptimisedBuildingsCuboidFinalDirectRejection();
 
-    
-    //Valeurs de règles à saisir
-    //Recul par rapport à la voirie
-    double distReculVoirie = 0.0;
-    //Recul par rapport au fond de la parcelle
-    double distReculFond = 2;
-    //Recul par rapport aux bordures latérales
-    double distReculLat = 4;
-    //Distance entre 2 boîtes d'une même parcelle
-    double distanceInterBati = 5;
-    //CES maximal (2 ça ne sert à rien)
-    double maximalCES = 2;
+		// Valeurs de règles à saisir
+		// Recul par rapport à la voirie
+		double distReculVoirie = 0.0;
+		// Recul par rapport au fond de la parcelle
+		double distReculFond = 2;
+		// Recul par rapport aux bordures latérales
+		double distReculLat = 4;
+		// Distance entre 2 boîtes d'une même parcelle
+		double distanceInterBati = 5;
+		// CES maximal (2 ça ne sert à rien)
+		double maximalCES = 2;
 
-    //Instanciation du sampler avec l'unité foncière et les valeurs ci-dessus 
-    SamplePredicate2<Cuboid, GraphConfiguration<Cuboid>, BirthDeathModification<Cuboid>> pred = new SamplePredicate2<>(
-        bPU, distReculVoirie, distReculFond, distReculLat, distanceInterBati,
-        maximalCES);
+		// Instanciation du sampler avec l'unité foncière et les valeurs
+		// ci-dessus
+		SamplePredicate2<Cuboid, GraphConfiguration<Cuboid>, BirthDeathModification<Cuboid>> pred = new SamplePredicate2<>(
+				bPU, distReculVoirie, distReculFond, distReculLat,
+				distanceInterBati, maximalCES);
 
-  
-    //Lancement de l'optimisation avec unité foncière, paramètres, environnement, id et prédicat
-    GraphConfiguration<Cuboid> cc = oCB.process(bPU, p, env, 1, pred);
+		// Lancement de l'optimisation avec unité foncière, paramètres,
+		// environnement, id et prédicat
+		GraphConfiguration<Cuboid> cc = oCB.process(bPU, p, env, 1, pred);
 
-    
-    //On prépare la sortie pour récupérer la liste des entités
-    IFeatureCollection<IFeature> iFeatC = new FT_FeatureCollection<>();
+		// On prépare la sortie pour récupérer la liste des entités
+		IFeatureCollection<IFeature> iFeatC = new FT_FeatureCollection<>();
 
-    for (GraphVertex<Cuboid> v : cc.getGraph().vertexSet()) {
+		for (GraphVertex<Cuboid> v : cc.getGraph().vertexSet()) {
 
-      IMultiSurface<IOrientableSurface> iMS = new GM_MultiSurface<>();
-      iMS.addAll(GenerateSolidFromCuboid.generate(v.getValue()).getFacesList());
+			IMultiSurface<IOrientableSurface> iMS = new GM_MultiSurface<>();
+			iMS.addAll(GenerateSolidFromCuboid.generate(v.getValue())
+					.getFacesList());
 
-      IFeature feat = new DefaultFeature(iMS);
-      //On ajoute des attributs aux entités (dimension des objets)
-      AttributeManager.addAttribute(feat, "Longueur",
-          Math.max(v.getValue().length, v.getValue().width), "Double");
-      AttributeManager.addAttribute(feat, "Largeur",
-          Math.min(v.getValue().length, v.getValue().width), "Double");
-      AttributeManager.addAttribute(feat, "Hauteur", v.getValue().height,
-          "Double");
-      AttributeManager.addAttribute(feat, "Rotation", v.getValue().orientation,
-          "Double");
+			IFeature feat = new DefaultFeature(iMS);
+			// On ajoute des attributs aux entités (dimension des objets)
+			AttributeManager
+					.addAttribute(feat, "Longueur",
+							Math.max(v.getValue().length, v.getValue().width),
+							"Double");
+			AttributeManager
+					.addAttribute(feat, "Largeur",
+							Math.min(v.getValue().length, v.getValue().width),
+							"Double");
+			AttributeManager.addAttribute(feat, "Hauteur", v.getValue().height,
+					"Double");
+			AttributeManager.addAttribute(feat, "Rotation",
+					v.getValue().orientation, "Double");
 
-      iFeatC.add(feat);
+			iFeatC.add(feat);
 
-    }
+		}
 
-    
-    //On écrit en sortie le shapefile
-    //ATTENTIONT : il faut mettre à jour le nom de fichier en sorie
-    ShapefileWriter.write(iFeatC, p.get("result").toString() + "out.shp");
+		// On écrit en sortie le shapefile
+		// ATTENTIONT : il faut mettre à jour le nom de fichier en sorie
+		ShapefileWriter.write(iFeatC, p.get("result").toString() + "out.shp");
 
-    System.out.println("That's all folks");
+		System.out.println("That's all folks");
 
-
-  }
+	}
 
 }
--- src/main/java/fr/ign/cogit/simplu3d/iauidf/Exec.java
+++ src/main/java/fr/ign/cogit/simplu3d/iauidf/Exec.java
@@ -1,19 +1,53 @@
 package fr.ign.cogit.simplu3d.iauidf;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
 
+import fr.ign.cogit.simplu3d.exec.BasicSimulator;
 import fr.ign.cogit.simplu3d.iauidf.regulation.Regulation;
+import fr.ign.cogit.simplu3d.importer.applicationClasses.RoadImporter;
+import fr.ign.cogit.simplu3d.io.load.application.LoaderSHP;
+import fr.ign.cogit.simplu3d.model.application.BasicPropertyUnit;
+import fr.ign.cogit.simplu3d.model.application.Environnement;
+import fr.ign.parameters.Parameters;
 
 public class Exec {
-	
-	
+
 	private static Logger log = Logger.getLogger(Exec.class);
 
-	public static void main(String[] args) throws IOException {
+	public final static String nomDTM = "MNT_BD3D.asc";
+	
+	
+	public static Parameters p;
+
+	// Initialisation des attributs différents du schéma de base
+	// et le fichier de paramètre commun à toutes les simulations
+	public static void init() throws Exception {
+
+		RoadImporter.ATT_NOM_RUE = "NOM_VOIE_G";
+		RoadImporter.ATT_LARGEUR = "LARGEUR";
+		RoadImporter.ATT_TYPE = "NATURE";
+		
+		
+		// Chargement du fichier de configuration
+		String folderName = BasicSimulator.class.getClassLoader()
+				.getResource("scenario/").getPath();
+		String fileName = "parameters_iauidf.xml";
+		p = Parameters.unmarshall(new File(folderName + fileName));
+	}
+	
+	
+	
+	
+
+	public static void main(String[] args) throws IOException,
+			CloneNotSupportedException {
 		// Dossier contenant IMU_MANTES_TEST.csv et les sous dossier par code
 		// IMU
 		String folder = "C:/Users/mbrasebin/Desktop/Ilots_test/COGIT78/";
@@ -23,41 +57,71 @@ public class Exec {
 		// réglements pour un code IMU du fait des bandes)
 		Map<Integer, List<Regulation>> mapReg = Regulation
 				.loadRegulationSet(csvFile);
-		
-		//Fonction de test : chargement de la réglementation :
+
+		// Fonction de test : chargement de la réglementation :
 		testLoadedRegulation(mapReg);
 
-		//On traite indépendamment chaque zone imu
-		for(int imu : mapReg.keySet()){
-			
-			boolean simul = simulRegulation(imu, mapReg.get(imu));
-			
-			if(! simul){
+		// @TODO : gérer les attributs indépendemment de la casse.
+		// Initialisation des noms d'attributs
+		init();
+
+		// On traite indépendamment chaque zone imu
+		for (int imu : mapReg.keySet()) {
+
+			boolean simul = simulRegulationByIMU(imu, mapReg.get(imu), folder
+					+ imu + "/");
+
+			if (!simul) {
 				log.warn("--Probleme pour la simulation : " + imu);
 			}
-					
+
 		}
-			
+
 	}
 
-	
 	/**
-	 * Simulations portant 
+	 * Simulations portant sur chaque zone IMU
+	 * 
+	 * @param imu
+	 * @param lReg
+	 * @return
+	 * @throws CloneNotSupportedException
+	 * @throws FileNotFoundException
+	 */
+	public static boolean simulRegulationByIMU(int imu, List<Regulation> lReg,
+			String folderImu) throws FileNotFoundException,
+			CloneNotSupportedException {
+
+		Environnement env = LoaderSHP.load(folderImu, new FileInputStream(
+				folderImu + nomDTM));
+
+		boolean isOk = true;
+
+		for (BasicPropertyUnit bPU : env.getBpU()) {
+
+			isOk = isOk && simulRegulationByBasicPropertyUnit(bPU, imu, lReg);
+
+		}
+
+		return true;
+	}
+
+	/**
+	 * 
+	 * @param bPU
 	 * @param imu
 	 * @param lReg
 	 * @return
 	 */
-	public static boolean simulRegulation(int imu, List<Regulation> lReg){
+	public static boolean simulRegulationByBasicPropertyUnit(
+			BasicPropertyUnit bPU, int imu, List<Regulation> lReg) {
 		
 		
-		
-		
-		
+
 		return true;
 	}
-	
-	
-	//Affiche les règlements chargés
+
+	// Affiche les règlements chargés
 	public static void testLoadedRegulation(
 			Map<Integer, List<Regulation>> mapReg) {
 
