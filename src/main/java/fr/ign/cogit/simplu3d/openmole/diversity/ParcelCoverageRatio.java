d3c5c612bc5176322b5b917c6d7b52e32c9d6188
julienperret <julien.perret@gmail.com>
2015-10-28 12:00:47 +0100

    Force geometries to 2D before the computation of the coverage ratio.

--- src/main/java/fr/ign/cogit/simplu3d/openmole/diversity/ParcelCoverageRatio.java
+++ src/main/java/fr/ign/cogit/simplu3d/openmole/diversity/ParcelCoverageRatio.java
@@ -3,12 +3,18 @@ package fr.ign.cogit.simplu3d.openmole.diversity;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.CoordinateFilter;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryFactory;
+
 import fr.ign.cogit.geoxygene.api.feature.IFeature;
 import fr.ign.cogit.geoxygene.api.feature.IFeatureCollection;
-import fr.ign.cogit.geoxygene.api.spatial.geomroot.IGeometry;
 import fr.ign.cogit.geoxygene.api.spatial.coordgeom.IPolygon;
+import fr.ign.cogit.geoxygene.api.spatial.geomroot.IGeometry;
 import fr.ign.cogit.geoxygene.spatial.geomaggr.GM_MultiSurface;
 import fr.ign.cogit.geoxygene.util.algo.JtsAlgorithms;
+import fr.ign.cogit.geoxygene.util.conversion.AdapterFactory;
 import fr.ign.cogit.simplu3d.io.ExportAsFeatureCollection;
 import fr.ign.cogit.simplu3d.model.application.BasicPropertyUnit;
 import fr.ign.cogit.simplu3d.rjmcmc.cuboid.geometry.impl.Cuboid;
@@ -32,10 +38,30 @@ public class ParcelCoverageRatio {
     for (IFeature f : collection) {
       list.add((GM_MultiSurface<IPolygon>) f.getGeom());
     }
-    if (list.isEmpty()) return 0.0;
+    // if there is no object, don't bother
+    if (list.isEmpty()) {
+      return 0.0;
+    }
     IGeometry union = JtsAlgorithms.union(list);
-    if (union == null || union.isEmpty()) return 0.0;
-    double buildingArea = union.area();
-    return buildingArea / area;
+    // if the union is empty, stop now
+    if (union == null || union.isEmpty()) {
+      return 0.0;
+    }
+    try {
+      // force the union to a 2D geometry (the input geometries are 3D)
+      Geometry g = AdapterFactory.toGeometry(new GeometryFactory(), union);
+      g.apply(new CoordinateFilter() {
+        @Override
+        public void filter(Coordinate coord) {
+          coord.setCoordinate(new Coordinate(coord.x, coord.y));
+        }
+      });
+      g = g.buffer(0);
+      double buildingArea = g.getArea();
+      return buildingArea / area;
+    } catch (Exception e) {
+      e.printStackTrace();
+      return 0.0;
+    }
   }
 }
