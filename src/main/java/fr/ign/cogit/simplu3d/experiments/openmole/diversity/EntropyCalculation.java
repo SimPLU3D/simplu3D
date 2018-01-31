6f44ff69e0685c61b3fc1f57cb2db038a19079a4
chapinux <paul.chapron@gmail.com>
2018-01-31 16:43:09 +0100

    debug computation of IFeatureCollection height entropy

--- src/main/java/fr/ign/cogit/simplu3d/experiments/openmole/diversity/EntropyCalculation.java
+++ src/main/java/fr/ign/cogit/simplu3d/experiments/openmole/diversity/EntropyCalculation.java
@@ -9,105 +9,159 @@ import javax.media.j3d.Sound;
 
 import java.util.HashMap;
 
-
 import java.lang.Math;
 import java.lang.reflect.Array;
 
+import fr.ign.cogit.geoxygene.api.feature.IFeature;
 import fr.ign.cogit.geoxygene.api.spatial.geomroot.IGeometry;
+import fr.ign.cogit.geoxygene.feature.FT_FeatureCollection;
+import fr.ign.cogit.geoxygene.sig3d.io.vector.ShapeFileLoader;
+import fr.ign.cogit.simplu3d.model.Building;
 import fr.ign.cogit.simplu3d.rjmcmc.cuboid.geometry.impl.Cuboid;
 import fr.ign.cogit.simplu3d.rjmcmc.cuboid.geometry.loader.LoaderCuboid;
 import fr.ign.mpp.configuration.GraphConfiguration;
+import javassist.bytecode.analysis.Subroutine;
 
 public class EntropyCalculation {
 
-  List<Cuboid> cuboids;
+  List<Cuboid> individualsCuboids;
+  List<IFeature> individualsFeatures;
 
   public EntropyCalculation(List<Cuboid> lCuboids) {
-    cuboids = lCuboids;
+    individualsCuboids = lCuboids;
   }
 
-  
   public EntropyCalculation(GraphConfiguration<Cuboid> graphCuboid) {
-    cuboids = new ArrayList<>();
+    individualsCuboids = new ArrayList<>();
     for (Cuboid cc : graphCuboid) {
-      cuboids.add(cc);
+      individualsCuboids.add(cc);
     }
   }
 
+  public EntropyCalculation(FT_FeatureCollection<IFeature> featColl) {
+    individualsFeatures = new ArrayList<>();
+    for (IFeature f : featColl) {
+      individualsFeatures.add(f);
+    }
 
-  public Double getHeightEntropy() {
-    return getHeightEntropy(1);
   }
-  
-  
-  public Double getHeightEntropy(Integer binwidth) {
+
+  public Double ShannonEntropyCuboidsHeight() {
+    return ShannonEntropyCuboidsHeight(1);
+  }
+
+  public Double ShannonEntropyCuboidsHeight(Integer binwidth) {
     HashMap<Double, Integer> histoHeights = new HashMap<Double, Integer>();
 
-
-    // populate hashmap according to rounded height 
-    for(Cuboid cucube : cuboids) {
+    // populate hashmap according to rounded height
+    for (Cuboid cucube : individualsCuboids) {
       Double height = cucube.getHeight();
-      Double  binarizedHeight =  (double)  Math.rint(height / binwidth) * binwidth;
-      
+      Double binarizedHeight = (double) Math.rint(height / binwidth) * binwidth;
+
       if (histoHeights.containsKey(binarizedHeight)) {
-        histoHeights.put(binarizedHeight, histoHeights.get(binarizedHeight) + 1);          
-      }
-      else {
+        histoHeights.put(binarizedHeight,
+            histoHeights.get(binarizedHeight) + 1);
+      } else {
         histoHeights.put(binarizedHeight, 1);
       }
     }
 
-
     // number of heights to consider = number of cuboids
-    int nbHeights= cuboids.size();
-    // number of distinct heights =  number of key in the hahmap
+    int nbHeights = individualsCuboids.size();
+    // number of distinct heights = number of key in the hahmap
     int nbDistinctHeights = histoHeights.keySet().size();
-    
-   
-    
-    //shannon entropy is -sum (pi log2(pi))
-    // probability of having an height h equal to H = number of H in hashmap / number of heights 
-    Double shannonE = 0.0 ;
-    for (Double k  : histoHeights.keySet()) {
-      Double probaHeight = (double)histoHeights.get(k) / (double)nbHeights ;
-      //  System.out.println("proba de la hauteur :"+k +" :" + probaHeight);
-      shannonE += shannonE + (probaHeight * Math.log10(probaHeight)/Math.log10(2));
+
+    // shannon entropy is -sum (pi log2(pi))
+    // probability of having an height h equal to H = number of H in hashmap /
+    // number of heights
+    Double shannonE = 0.0;
+    for (Double k : histoHeights.keySet()) {
+      Double probaHeight = (double) histoHeights.get(k) / (double) nbHeights;
+      shannonE += (probaHeight * (Math.log(probaHeight) / Math.log(2)));
     }
 
-    shannonE *= -1 ; 
-    
+    shannonE *= -1;
 
-    if (cuboids.isEmpty()) {
+    if (individualsCuboids.isEmpty()) {
       System.out.println("Empty list of cuboids ! ");
-      return -999.999 ;
-    }
-    else {
-      return shannonE ;  
+      return -999.999;
+    } else {
+      return shannonE;
     }
 
   }
 
-  
-  public static void main(String[] args) {
-System.out.println("load config");
-    List<Cuboid> lC = LoaderCuboid.loadFromShapeFile("/home/paulchapron/dev/simplu3D-openmole/visuPSE/PSEshp/run_-2085940094869048084out.shp");
-  //  List<Cuboid> lC = LoaderCuboid.loadFromShapeFile("/home/paulchapron/dev/simplu3D-openmole/visuPSE/config.shp");
-    System.out.println("load surrounding buildings");
-    List<Cuboid> surroundingFabric = LoaderCuboid.loadFromShapeFile("/home/paulchapron/dev/visuPSEPremium/scriptQGIS_genThree.js/Bati_zone/batiment.shp");
-     
-    
-    
-    
-    System.out.println("calcul de l'entropie des hauteurs de la config");
-    EntropyCalculation eC = new EntropyCalculation(lC);
-    double configEntropy = eC.getHeightEntropy(2);
+  public Double ShannonEntropyFeaturesHeight() {
+    return ShannonEntropyFeaturesHeight(1);
+  }
 
-    EntropyCalculation ecSurround = new EntropyCalculation(surroundingFabric) ;
-    Double surroundEntropy = ecSurround.getHeightEntropy();
-    
+  public Double ShannonEntropyFeaturesHeight(Integer binwidth) {
+    HashMap<Double, Integer> histoHeights = new HashMap<Double, Integer>();
+
+    // populate hashmap according to rounded height
+    for (IFeature batiment : individualsFeatures) {
+      Double height = ((Long) batiment.getAttribute("hauteur")).doubleValue();
+
+      Double binarizedHeight = (double) Math.rint(height / binwidth) * binwidth;
+      if (histoHeights.containsKey(binarizedHeight)) {
+        histoHeights.put(binarizedHeight,
+            histoHeights.get(binarizedHeight) + 1);
+      } else {
+        histoHeights.put(binarizedHeight, 1);
+      }
+    }
+
+    // number of heights to consider = number of cuboids
+    int nbHeights = individualsFeatures.size();
+
+    // shannon entropy is -sum (pi log2(pi))
+    // probability of having an height h equal to H = number of H in hashmap /
+    // number of heights
+    Double shannonE = 0.0;
+    for (Double k : histoHeights.keySet()) {
+      Double probaHeight = (double) histoHeights.get(k) / (double) nbHeights;
+
+      shannonE += probaHeight * (Math.log(probaHeight) / Math.log(2));
+    }
+
+    shannonE *= -1;
+    //System.out.println(histoHeights.keySet().size() + "hauteurs distinctes");
+
+    if (individualsFeatures.isEmpty()) {
+      System.out.println("Empty list of cuboids ! ");
+      return -999.999;
+    } else {
+      return shannonE;
+    }
+
+  }
+
+  public static void main(String[] args) {
+    System.out.println("load config");
+    List<Cuboid> lC = LoaderCuboid.loadFromShapeFile(
+        "/home/paulchapron/dev/simplu3D-openmole/visuPSE/PSEshp/run_-2085940094869048084out.shp");
+    // List<Cuboid> lC =
+    // LoaderCuboid.loadFromShapeFile("/home/paulchapron/dev/simplu3D-openmole/visuPSE/config.shp");
+    System.out.println("load surrounding buildings");
+
+    FT_FeatureCollection<IFeature> surroundingFabric = ShapeFileLoader
+        .loadingShapeFile(
+            "/home/paulchapron/dev/visuPSEPremium/scriptQGIS_genThree.js/Bati_zone/batiment.shp",
+            "", "hauteur", true);
+
+    EntropyCalculation eC = new EntropyCalculation(lC);
+    double configEntropy = eC.ShannonEntropyCuboidsHeight();
     System.out.println("Entropie des hauteurs de la config: " + configEntropy);
-    System.out.println("Entropie des hauteurs du tissu envirronant: " + surroundEntropy);
+
+    EntropyCalculation ecSurround = new EntropyCalculation(surroundingFabric);
+    Double surroundEntropy = ecSurround.ShannonEntropyFeaturesHeight();
+
+    System.out.println(surroundingFabric.size() + " batiments environnants");
+    System.out.println(
+        "Entropie des hauteurs de "+ surroundingFabric.size() + " batiments environnants : "+ surroundEntropy);
+
     
+    System.exit(0);
     
   }
 
